7a0ec1ed104c4c0f53ebecb91457cbc9ab113513
Tarek Adam <tarek.adam@gmail.com>
2019-05-23 10:57:28 -0600

    videos

---
+++ src/components/Videos.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <!-- 16:9 aspect ratio embed-responsive-16by9 -->
+        <!-- 4:3 aspect ratio embed-responsive-4by3 -->
+        <div class="embed-responsive embed-responsive-16by9">
+            <video controls playsinline>
+                <source v-for="url in video" :src="url" :type="mimeType(url)">
+            </video>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    import {mapActions} from 'vuex'
+
+    export default {
+        name: 'videos',
+        props: ['video'],
+
+        methods: {
+
+            mimeType: (url) => {
+                let mime = url.split('?')[0].split('.').pop()
+                return {
+                    mp4: 'video/mp4',
+                    webm: 'video/webm'
+                }[mime]
+            },
+
+            ...mapActions([
+                'setModal'
+            ])
+        }
+    }
+</script>
+
+<style scoped>
+    .noshare {
+        display: block;
+        width: 30%;
+        height: 100%;
+        position: absolute;
+        top: +0px;
+        right: +0px;
+        z-index: 1000;
+
+    }
+</style>
