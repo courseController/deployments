b4c5a9fc6b8fa3c3b3e3b8f4e1e180767f6961de
Tarek Adam <tarek.adam@gmail.com>
2019-06-14 15:38:37 -0600

    hell ya!!

--- src/components/Videos.vue
+++ src/components/Videos.vue
@@ -1,7 +1,4 @@
 <template>
-    <div>
-        <!-- 16:9 aspect ratio embed-responsive-16by9 -->
-        <!-- 4:3 aspect ratio embed-responsive-4by3 -->
         <div class="embed-responsive embed-responsive-16by9">
             <video controls playsinline>
                 <source v-for="url in video"
@@ -11,7 +8,6 @@
             </video>
         </div>
 
-    </div>
 </template>
 
 <script>
@@ -39,14 +35,10 @@
 </script>
 
 <style scoped>
-    .noshare {
-        display: block;
-        width: 30%;
-        height: 100%;
-        position: absolute;
-        top: +0px;
-        right: +0px;
-        z-index: 1000;
-
+    .embed-responsive{
+        background-image: url("/bg_video.jpg");
+        background-repeat: no-repeat;
+        background-position: center center;
+        background-size: cover;
     }
 </style>
